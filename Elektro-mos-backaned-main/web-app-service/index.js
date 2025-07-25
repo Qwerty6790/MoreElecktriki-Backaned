f0fb38d1172fc3668e2b99def99e275eacf24c8a
Qwerty <houldcould78@mail.ru>
2025-07-25 19:22:39 +0300

    text

--- Elektro-mos-backaned-main/web-app-service/index.js
+++ Elektro-mos-backaned-main/web-app-service/index.js
@@ -20,7 +20,12 @@ const PORT = process.env.PORT || 3007; // Устанавливаем порт д
 connectDB(); // Подключаемся к базе данных
 
 app.use(express.json()); // Настраиваем middleware для обработки JSON в запросах
-app.use(cors()); // Настраиваем CORS
+app.use(cors({
+    origin: ['http://178.209.127.237', 'http://localhost:3000', 'http://localhost:3001'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+}));
 
 // Настраиваем маршруты с префиксом /api
 app.use('/api', authRouter);    
