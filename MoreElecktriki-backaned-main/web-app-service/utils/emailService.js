5bc52377980123d8b7054d520fc6660559080da2
Qwerty <houldcould78@mail.ru>
2026-03-14 20:03:38 +0300

    text

--- MoreElecktriki-backaned-main/web-app-service/utils/emailService.js
+++ MoreElecktriki-backaned-main/web-app-service/utils/emailService.js
@@ -7,7 +7,7 @@ const transporter = nodemailer.createTransport({
     secure: false, // true для 465, false для других портов
     auth: {
         user: 'moreelektriki@gmail.com',
-        pass: 'snah sgvw mahs sbwd' // Замените на ваш App Password
+        pass: 'nekr wctc armk vwoe' // Замените на ваш App Password
     },
     tls: {
         rejectUnauthorized: false
